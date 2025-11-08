b561c4bc08697bad3e168a7b99fd7e3e48af850c
Joshua Clark <adonismytruename@gmail.com>
2025-11-07 16:33:32 -0800

    refined structure, did some testing

--- GodotFiles/scripts/game.gd
+++ GodotFiles/scripts/game.gd
@@ -2,15 +2,18 @@ extends Node2D
 
 
 @export var main_menu : PackedScene
+@export var entrance : PackedScene
 
-var current_screen : Screen
+var current_screen : Screen = null
 
 
-var screens = {
-	"main_menu": main_menu
-}
+var screens = {}
 
 func _ready() -> void:
+	screens = {
+			"main_menu": main_menu,
+			"entrance": entrance
+	}
 	_change_screen("main_menu")
 	
 
@@ -23,6 +26,7 @@ func _change_screen(screen : String) -> void:
 		remove_child(current_screen)
 		current_screen.queue_free()
 	
+	print_debug(screens[screen])
 	var new_screen : Screen = screens[screen].instantiate()
 	add_child(new_screen)
 	current_screen = new_screen
--- GodotFiles/scripts/main_menu.gd
+++ GodotFiles/scripts/main_menu.gd
@@ -3,9 +3,13 @@ extends Screen
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var bttn : Button = Button.new()
+	bttn.text = "start"
+	bttn.pressed.connect(_on_button_pressed)
+	add_child(bttn)
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_button_pressed() -> void:
+	change_screen.emit("entrance")
+	
