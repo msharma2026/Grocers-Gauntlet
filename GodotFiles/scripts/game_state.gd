2ee699f61ee1bda1ffca65935e0754c9e455bf4d
Marq Lott <mclott@ucdavis.edu>
2025-11-07 23:58:20 -0800

    exported dictionary for screens. Can now freely add packed scenes from inspector panel. Also created a way to add buttons throught the inspector panel in the main menu scene

--- GodotFiles/scripts/game_state.gd
+++ GodotFiles/scripts/game_state.gd
@@ -1,1 +1,4 @@
 class_name GameState
+extends Node2D
+
+var health_percent: int
--- GodotFiles/scripts/main_menu.gd
+++ GodotFiles/scripts/main_menu.gd
@@ -1,15 +1,22 @@
 extends Screen
 
+# Button map holds the button's display name (Key) and the destination screen ID (Value).
+@export var button_map: Dictionary[String, String]
+
+var button: Array[Button]
+var index: int = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var bttn : Button = Button.new()
-	bttn.text = "start"
-	bttn.pressed.connect(_on_button_pressed)
-	add_child(bttn)
+	for button_id in button_map.keys():
+		var button_instance = Button.new()
+		button_instance.text = button_id
+		button_instance.pressed.connect(_on_button_pressed.bind(button_map[button_id]))
+		add_child(button_instance)
+		index += 1
 	
-
-
-func _on_button_pressed() -> void:
-	change_screen.emit("entrance")
+	
+func _on_button_pressed(screen_id: String) -> void:
+	change_screen.emit(screen_id)
 	
