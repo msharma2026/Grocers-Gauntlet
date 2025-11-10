2fd52e887bd33328b693bfd0230eeb556126d49a
Joshua Clark <adonismytruename@gmail.com>
2025-11-10 04:33:20 -0800

    continued work on Aisles logic

--- GodotFiles/scripts/aisle.gd
+++ GodotFiles/scripts/aisle.gd
@@ -1,24 +1,27 @@
 extends Area2D
 
+signal aisle_clicked(screen_id: String)
 
-# Called when the node enters the scene tree for the first time.
+@export var screen_id: String = ""
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
 func _ready() -> void:
-	connect("input_event",Callable(self,"_on_click"))
-	connect("mouse_entered",Callable(self,"_on_hover"))
-	connect("mouse_exited",Callable(self,"_revert_sprite"))
+	connect("input_event", Callable(self, "_on_click"))
+	connect("mouse_entered", Callable(self, "_on_hover"))
+	connect("mouse_exited", Callable(self, "_revert_sprite"))
+	animated_sprite.play("default")
 
 
-func _on_click(viewport: Node, event: InputEvent, shape_idx: int ) -> void:
-	if event is InputEventMouseButton and event.button_inex == MOUSE_BUTTON_LEFT and event.pressed:
-		pass
-	# TODO: send a signal out when clicked so Aisles can emit screen change
-	
+func _on_click(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		aisle_clicked.emit(screen_id)
+
 
 func _on_hover() -> void:
-	pass
-	# TODO: change sprite to the "hovering"
+	animated_sprite.play("hovering")
 
 
 func _revert_sprite() -> void:
-	pass
-	# TODO: go back to "default" sprite after hovering ends
+	animated_sprite.play("default")
--- GodotFiles/scripts/aisles.gd
+++ GodotFiles/scripts/aisles.gd
@@ -1,12 +1,43 @@
 class_name Aisles
 extends Screen
 
+# Aisle weights for random selection
+# Higher weight means higher probability of selection
+var aisle_weights = {
+    "Haggle_Dairy": 3,
+    "Haggle_Meat": 3,
+    "Haggle_Weapons": 2,
+    "Haggle_Potions": 2,
+    "Haggle_Treasure": 1,
+    "Special_Event": 2,
+    "Black_Market": 1
+}
+
+var pool_of_aisles: Array[String] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var rng = RandomNumberGenerator.new()
+	var num_aisles = rng.randi_range(1, 3)
 
+	for i in range(num_aisles):
+		pool_of_aisles.append(get_random_aisle())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func get_random_aisle() -> String:
+	var total_weight = 0
+	for weight in aisle_weights.values():
+		total_weight += weight
+	
+	var random_value = randi() % total_weight
+	var cumulative_weight = 0
+	
+	for aisle_name in aisle_weights.keys():
+		cumulative_weight += aisle_weights[aisle_name]
+		if random_value < cumulative_weight:
+			return aisle_name
+	
+	return ""  
