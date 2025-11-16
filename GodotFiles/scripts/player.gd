660e7c01e12b44a33546a44ba851ac9adfacc2c7
David Estrella <dtestrella@ucdavis.edu>
2025-11-16 15:40:28 -0800

    Hooked up player with the health bar

--- GodotFiles/scripts/player.gd
+++ GodotFiles/scripts/player.gd
@@ -1,13 +1,29 @@
-extends Node2D
+#player.gd
+class_name Player
+extends CharacterBody2D
+
+@export var health: float = 100.0
+@export var max_health: float = 100.0
+
+signal health_updated(new_health, max_health)
 
 @onready var _player: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _ready() -> void:
 	pass
 	
+	
 func _process(delta: float) -> void:
 	pass
+	
 	
 func _physics_process(delta: float) -> void:
 	pass
 	
+
+func take_damage(amount: float) -> void:
+	health -= amount
+	# Prevents health being negative
+	health = clampf(health, 0, max_health)
+	health_updated.emit(health, max_health)
