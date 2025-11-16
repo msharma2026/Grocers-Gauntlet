01271bed5e3bc73e9ecb18e1cfe33cfa4ebb3963
David Estrella <dtestrella@ucdavis.edu>
2025-11-15 18:56:09 -0800

    Added a quit button for the main menu and implemented the pause menu

--- GodotFiles/scripts/game.gd
+++ GodotFiles/scripts/game.gd
@@ -9,15 +9,58 @@ extends Node2D
 # Inspector panel for 'main_menu.tscn' under button_map dictionary.
 
 @export var screens : Dictionary[String, PackedScene]
+@export var pause_menu_scene : PackedScene
 
 var current_screen : Screen
+var pause_menu_instance : PauseMenu = null
 
 
 func _ready() -> void:
 	_change_screen("main_menu")
 	
+# Checks if 'Escape' key was pressed to bring up the pause menu
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause_menu"):
+		if pause_menu_instance != null:
+			_resume_game()
+		else:
+			get_tree().paused = true
+			pause_menu_instance = pause_menu_scene.instantiate()
+			pause_menu_instance.resume_game.connect(_resume_game)
+			pause_menu_instance.quit_to_main_menu.connect(_quit_to_main_menu)
+			pause_menu_instance.quit_game.connect(_quit_game)
+			add_child(pause_menu_instance)
+
+
+func _resume_game() -> void:
+	# Unpauses the game
+	if pause_menu_instance != null:
+		get_tree().paused = false
+		pause_menu_instance.queue_free()
+		pause_menu_instance = null
+		
+
+func _quit_to_main_menu() -> void:
+	# Returns to main menu
+	get_tree().paused = false
+	
+	if pause_menu_instance != null:
+		pause_menu_instance.queue_free()
+		pause_menu_instance = null
+		
+	_change_screen("main_menu")
+
+
+func _quit_game() -> void:
+	# Closes the game
+	get_tree().quit()
+
 
 func _change_screen(screen_id: String) -> void:
+	# Makes clicking the 'Exit Game' button quit the game
+	if screen_id == "quit":
+		_quit_game()
+		return
 	
 	if !screens.has(screen_id):
 		push_warning("Unknown screen_id: %s" % screen_id)
@@ -26,6 +69,7 @@ func _change_screen(screen_id: String) -> void:
 	if current_screen != null:
 		remove_child(current_screen)
 		current_screen.queue_free()
+		
 
 	print_debug(screens[screen_id])
 	var new_screen : Screen = screens[screen_id].instantiate()
--- GodotFiles/scripts/pause_menu.gd
+++ GodotFiles/scripts/pause_menu.gd
@@ -2,3 +2,31 @@
 
 class_name PauseMenu
 extends Screen
+
+signal resume_game
+signal quit_to_main_menu
+signal quit_game
+
+var button: Array[Button]
+var current_y: int = 100
+
+@export var button_map: Dictionary[String, String]
+# Creates buttons for Resume, Main Menu, and Exit Game
+func _ready() -> void:
+	for button_id in button_map.keys():
+		var button_instance = Button.new()
+		button_instance.position.y = current_y
+		button_instance.text = button_id
+		button_instance.pressed.connect(_on_button_pressed.bind(button_map[button_id]))
+		add_child(button_instance)
+		
+		current_y += 50
+
+# Emits signal for _unhandled_input() in game.gd to listen to
+func _on_button_pressed(action_id: String) -> void:
+	if action_id == "resume":
+		resume_game.emit()
+	elif action_id == "main_menu":
+		quit_to_main_menu.emit()
+	elif action_id == "quit":
+		quit_game.emit()
