bf21ccc2def0ca248394be4bcc806ec29f0baf4c
Marq Lott <mclott@ucdavis.edu>
2025-11-16 15:27:19 -0800

    created blank player scene and movement commands

--- GodotFiles/scripts/player.gd
+++ GodotFiles/scripts/player.gd
@@ -1,1 +1,13 @@
-extends AnimatedSprite2D
+extends Node2D
+
+@onready var _player: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	pass
+	
