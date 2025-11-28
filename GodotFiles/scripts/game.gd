cb04a8a5076e326458001f83be0a137e34389e8e
Marq Lott <mclott@ucdavis.edu>
2025-11-27 16:55:54 -0800

    fixed pause menu logic for when to show

--- GodotFiles/scripts/game.gd
+++ GodotFiles/scripts/game.gd
@@ -46,9 +46,7 @@ func _process(delta: float) -> void:
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause_menu") and \
-			current_screen is not MainMenu and \
-			current_screen is not Entrance and \
-			current_screen is not Inventory:
+			current_screen is Aisles:
 		if pause_menu_instance != null:
 			_resume_game()
 		else:
